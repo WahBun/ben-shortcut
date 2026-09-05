af31ac610fcfc4e16ee63546e9da240218c868cb
WahBun <76650587+WahBun@users.noreply.github.com>
2026-09-06 02:17:45 +0800

    Improve beginner terms and guide scrolling

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "pulse-rc-github-pages-v31";
-const OPTION_TRAINER_VERSION = "ost-v31";
+const CACHE_NAME = "pulse-rc-github-pages-v32";
+const OPTION_TRAINER_VERSION = "ost-v32";
 const APP_SHELL = [
   "./",
   "./index.html",
